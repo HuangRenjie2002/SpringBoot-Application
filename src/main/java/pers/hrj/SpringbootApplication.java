fb98abffa4d414635a916666a2c6ee1a6e35e3f3
HuangRenjie <1090028720@qq.com>
2023-08-01 10:56:53 +0800

    init

---
+++ src/main/java/pers/hrj/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package pers.hrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
