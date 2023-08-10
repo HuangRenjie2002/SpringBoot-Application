fd2e48a2ccf3fea3f4667207f42518421ae1f234
HuangRenjie <huangrenjie2002@qq.com>
2023-08-10 16:02:05 +0800

    RabbitMQ发送消息设置过期时间

--- src/main/java/pers/hrj/rabbitmq/RabbitMQController.java
+++ src/main/java/pers/hrj/rabbitmq/RabbitMQController.java
@@ -1,5 +1,9 @@
 package pers.hrj.rabbitmq;
 
+import com.alibaba.fastjson2.JSON;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import pers.hrj.model.entity.User;
 import pers.hrj.model.vo.SimpleRestVo;
 import pers.hrj.service.UserService;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/rabbitMq")
@@ -22,7 +29,12 @@ public class RabbitMQController {
         User user = userService.getBaseMapper().selectById(1686311752046559233L);
 //        rabbitTemplate.convertAndSend("Exchange","test",user);
 
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setExpiration("10000");
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+        Message message = new Message(JSON.toJSONString(user).getBytes(StandardCharsets.UTF_8), messageProperties);
 
+        rabbitTemplate.convertAndSend("Exchange","test",message,correlationData);
         return SimpleRestVo.successVo();
     }
 }
--- src/main/java/pers/hrj/rabbitmq/RabbitMQListener.java
+++ src/main/java/pers/hrj/rabbitmq/RabbitMQListener.java
@@ -1,17 +1,16 @@
 package pers.hrj.rabbitmq;
 
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-import pers.hrj.model.entity.User;
 
 @Component
 @RabbitListener(queues = "Queue")
 public class RabbitMQListener {
-
-    @RabbitHandler
-    public void handler(User user){
-        System.out.println(user);
-    }
+//
+//    @RabbitHandler
+//    public void handler(Message message){
+//        byte[] body = message.getBody();
+//        System.out.println(body);
+//    }
 
 }
